07782639201aa47a5d89b0d3c2133d326d48e46e
Chaoran Lu <andyluloveusc@gmail.com>
2026-04-23 22:58:58 -0700

    feat: P0-2 Set up GraphQL in code-first mode for the NestJS application

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+type GraphQLHelloResponse = {
+  data: {
+    hello: string;
+  };
+};
+
+describe('GraphQL App (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,10 +21,28 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/graphql returns hello query result', async () => {
+    const query = {
+      query: `
+        query {
+          hello
+        }
+      `,
+    };
+
+    const server = app.getHttpServer() as Parameters<typeof request>[0];
+
+    const response = await request(server)
+      .post('/graphql')
+      .send(query)
+      .expect(200);
+
+    const body = response.body as GraphQLHelloResponse;
+
+    expect(body.data.hello).toBe('GraphQL is ready');
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
